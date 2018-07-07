1d26e3a52998880eb3686d3713462ae7f6c2c25b
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2018-07-06 23:53:21 -0500

    Juego, casi terminado

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,30 +1,41 @@
-import sys
 import pygame
 import game_functions as gf
-from Settings import Settings
+from settings import Settings
 from pygame.sprite import Group
 from ship import Ship
+#from alien import Alien
+from game_stats import GameStats
+from button import Bottom
+
 
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_heigth))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
+    #alien = Alien(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+    stats = GameStats(ai_settings)
     bg_color = ai_settings.bg_color
-
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    play_button = Bottom(ai_settings,screen, "PLAY")
     while True:
+        #update postions to draw a new screen
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
         bullets.update()
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            # update positon of the ship
+            ship.update()
+            # delete bullets from game
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        #check player input
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
 
         screen.fill(bg_color)
         ship.blitme()
